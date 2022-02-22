1523e6f8d633781fc38b5c650f1383012fc5cf70
Yoann <yoann.deblois@gmail.com>
2022-02-22 16:04:43 +0100

    func: added web_api url from django_rest_framework.

--- epic_events/epic_events/urls.py
+++ epic_events/epic_events/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
