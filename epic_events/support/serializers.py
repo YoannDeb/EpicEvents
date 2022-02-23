fd04a4e326a6664c99c34f1df817b24be29531fb
Yoann <yoann.deblois@gmail.com>
2022-02-23 20:45:26 +0100

    bugfix: renamed event serializers EventSerializer and CreateEventSerializer.

--- epic_events/support/serializers.py
+++ epic_events/support/serializers.py
@@ -3,13 +3,13 @@ from rest_framework import serializers
 from .models import Event
 
 
-class ContractSerializer(serializers.ModelSerializer):
+class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = ['id', 'support_contact', 'client', 'date_created', 'date_updated', 'status', 'attendees', 'event_date', 'notes']
 
 
-class CreateContractSerializer(serializers.ModelSerializer):
+class CreateEventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = ['support_contact', 'client', 'status', 'attendees', 'event_date', 'notes']
