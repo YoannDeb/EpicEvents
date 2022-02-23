fafd12cea4e9d14975273bb287ddb0044439403f
Yoann <yoann.deblois@gmail.com>
2022-02-23 20:46:16 +0100

    func: First commit of sales view, WIP.

--- epic_events/sales/views.py
+++ epic_events/sales/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from .models import Contract
+from .serializers import ContractSerializer
+
+
+class ContractViewSet(viewsets.ModelViewSet):
+    serializer_class = ContractSerializer
+
+    def get_permissions(self):
+        permission_classes = [permissions.IsAuthenticated()]
+        return permission_classes
+
+    def get_queryset(self):
+        return Contract.objects.all()
