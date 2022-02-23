72944edb508e19996964de047515fc8521b26f16
Yoann <yoann.deblois@gmail.com>
2022-02-23 20:42:30 +0100

    func: admin settings for Contracts.

--- epic_events/sales/admin.py
+++ epic_events/sales/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Contract
 
-# Register your models here.
+
+class ContractAdmin(admin.ModelAdmin):
+    list_display = ('client', 'sales_contact', 'date_created', 'date_updated', 'status', 'amount', 'payment_due')
+
+
+admin.site.register(Contract, ContractAdmin)
