84c77344163f4f8d49c78c1379deecb1ef999d92
Yoann <yoann.deblois@gmail.com>
2022-02-23 20:43:21 +0100

    func: admin settings for Events.

--- epic_events/support/admin.py
+++ epic_events/support/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Event
 
-# Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('client', 'support_contact', 'date_created', 'date_updated', 'status', 'attendees', 'event_date', 'notes')
+
+
+admin.site.register(Event, EventAdmin)
