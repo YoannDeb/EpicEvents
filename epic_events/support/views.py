9893592f7bc52b38ab3222c97d8a31fbf36fa705
Yoann <yoann.deblois@gmail.com>
2022-02-23 20:46:29 +0100

    func: First commit of support view, WIP.

--- epic_events/support/views.py
+++ epic_events/support/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from .models import Event
+from .serializers import EventSerializer
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    serializer_class = EventSerializer
+
+    def get_permissions(self):
+        permission_classes = [permissions.IsAuthenticated()]
+        return permission_classes
+
+    def get_queryset(self):
+        return Event.objects.all()
+
