2f0cd94bd0cb445c2235245f4607e432909162af
Yoann <yoann.deblois@gmail.com>
2022-02-23 20:45:59 +0100

    func: First commit of authentication views, WIP.

--- epic_events/authentification/views.py
+++ epic_events/authentification/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from .models import Client, CustomUser
+from .serializers import ClientSerializer, UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        permission_classes = [permissions.IsAuthenticated]
+        return permission_classes
+
+    def get_queryset(self):
+        return CustomUser.objects.all()
+
+
+class ClientViewSet(viewsets.ModelViewSet):
+    serializer_class = ClientSerializer
+
+    def get_permissions(self):
+        permission_classes = [permissions.IsAuthenticated()]
+        return permission_classes
+
+    def get_queryset(self):
+        return Client.objects.all()
+
