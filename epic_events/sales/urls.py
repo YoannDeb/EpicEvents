1e03f203efbd32822744f4677a2e4c8ac3e78cad
Yoann <yoann.deblois@gmail.com>
2022-03-02 19:30:47 +0100

    func: urls for sales app.

---
+++ epic_events/sales/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import ContractViewSet
+
+router = routers.DefaultRouter()
+router.register(r'contracts', ContractViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
