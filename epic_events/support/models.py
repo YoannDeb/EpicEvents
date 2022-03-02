d9897c1e55ef9a264abf102ba838cbee13143562
Yoann <yoann.deblois@gmail.com>
2022-03-02 19:51:21 +0100

    func: Choices instead of boolean for events status

--- epic_events/support/models.py
+++ epic_events/support/models.py
@@ -9,11 +9,29 @@ class Event(models.Model):
     - A sale user
     - A client.
     """
+
+    FUTURE = 'FT'
+    ONGOING = 'OG'
+    FINISHED = 'FI'
+
+    STATUS_CHOICES = [
+        (FUTURE, 'Future'),
+        (ONGOING, 'Ongoing'),
+        (FINISHED, 'Finished')
+    ]
+
     client = models.ForeignKey(blank=True, null=True, to='authentification.CLIENT', on_delete=models.SET_NULL)
     support_contact = models.ForeignKey(blank=True, null=True, to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    status = models.BooleanField(default=False)
+    status = models.CharField(
+        default=FUTURE, max_length=2, choices=STATUS_CHOICES,
+        error_messages={
+            'invalid_choice': f'Type must be between those choices: '
+                              f'{FUTURE} for future events, {ONGOING} for ongoing events and '
+                              f'{FINISHED} for finished events'
+        }
+    )
     attendees = models.IntegerField(blank=True)
     event_date = models.DateTimeField(blank=True)
     notes = models.TextField(max_length=5000, blank=True)
