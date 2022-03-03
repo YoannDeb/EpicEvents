83082a6d2a7ba7c0a3aad466293dc5d8654491fe
Yoann <yoann.deblois@gmail.com>
2022-03-03 14:21:05 +0100

    func: sales_contact removed from serializer after removal in contract model

--- epic_events/sales/serializers.py
+++ epic_events/sales/serializers.py
@@ -6,10 +6,10 @@ from .models import Contract
 class ContractSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contract
-        fields = ['id', 'sales_contact', 'client', 'date_created', 'date_updated', 'status', 'amount', 'payment_due']
+        fields = ['id', 'client', 'date_created', 'date_updated', 'status', 'amount', 'payment_due']
 
 
 class CreateContractSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contract
-        fields = ['sales_contact', 'client', 'status', 'amount', 'payment_due']
+        fields = ['client', 'status', 'amount', 'payment_due']
