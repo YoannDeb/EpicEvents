3b74a7e683278db9ed9f8da11742682de6bd6092
Yoann <yoann.deblois@gmail.com>
2022-03-03 14:21:52 +0100

    func: permissions checking in views

--- epic_events/authentification/views.py
+++ epic_events/authentification/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, permissions
 
 from .models import Client, CustomUser
 from .serializers import ClientSerializer, UserSerializer
+from .permissions import IsClientResponsible, IsInSalesTeam
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -13,4 +14,13 @@ class UserViewSet(viewsets.ModelViewSet):
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+    def get_permissions(self):
+        permission_classes = [permissions.IsAuthenticated]
+        if self.action == 'retrieve' or self.action == 'list':
+            permission_classes = [permissions.IsAuthenticated]
+        if self.action == 'create':
+            permission_classes = [permissions.IsAuthenticated, IsInSalesTeam]
+        if self.action == 'destroy' or self.action == 'update' or self.action == 'partial_update':
+            permission_classes = [permissions.IsAuthenticated, IsClientResponsible]
+        return permission_classes
