fa304ab93b8215842b449247c1c7811a593bd176
Yoann <yoann.deblois@gmail.com>
2022-03-03 14:22:16 +0100

    func: permissions checking in views

--- epic_events/support/views.py
+++ epic_events/support/views.py
@@ -2,10 +2,19 @@ from rest_framework import viewsets, permissions
 
 from .models import Event
 from .serializers import EventSerializer
+from authentification.permissions import IsInSalesTeam, IsEventResponsible, IsEventSClientResponsible
 
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
+    def get_permissions(self):
+        permission_classes = [permissions.IsAuthenticated()]
+        if self.action == 'retrieve' or self.action == 'list':
+            permission_classes = [permissions.IsAuthenticated()]
+        if self.action == 'create':
+            permission_classes = [permissions.IsAuthenticated(), IsInSalesTeam()]
+        if self.action == 'destroy' or self.action == 'update' or self.action == 'partial_update':
+            permissions_classes = [permissions.IsAuthenticated(), IsEventResponsible()]
+        return permission_classes
