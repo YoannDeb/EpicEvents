25bf4b67e9a21af6877ca2e7ead552a5733ea7ec
Yoann <yoann.deblois@gmail.com>
2022-03-03 14:18:33 +0100

    func: sales_contact removed of model as it is redundant.

--- epic_events/sales/models.py
+++ epic_events/sales/models.py
@@ -10,7 +10,6 @@ class Contract(models.Model):
     - A client.
     """
     client = models.ForeignKey(blank=True, null=True, to='authentification.CLIENT', on_delete=models.SET_NULL)
-    sales_contact = models.ForeignKey(blank=True, null=True, to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     status = models.BooleanField(default=False)
