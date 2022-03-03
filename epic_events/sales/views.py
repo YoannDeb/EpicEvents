b4e60cd6627489e93606700b5bf5ab08e3f5ce5e
Yoann <yoann.deblois@gmail.com>
2022-03-03 14:22:04 +0100

    func: permissions checking in views

--- epic_events/sales/views.py
+++ epic_events/sales/views.py
@@ -2,9 +2,19 @@ from rest_framework import viewsets, permissions
 
 from .models import Contract
 from .serializers import ContractSerializer
+from authentification.permissions import IsInSalesTeam, IsContractSClientResponsible
 
 
 class ContractViewSet(viewsets.ModelViewSet):
     queryset = Contract.objects.all()
     serializer_class = ContractSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+    def get_permissions(self):
+        permission_classes = [permissions.IsAuthenticated()]
+        if self.action == 'retrieve' or self.action == 'list':
+            permission_classes = [permissions.IsAuthenticated()]
+        if self.action == 'create':
+            permission_classes = [permissions.IsAuthenticated(), IsInSalesTeam()]
+        if self.action == 'destroy' or self.action == 'update' or self.action == 'partial_update':
+            permissions_classes = [permissions.IsAuthenticated(), IsContractSClientResponsible()]
+        return permission_classes
