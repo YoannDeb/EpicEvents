e46975b22cf4f4379658b85de0d8b07c473936df
Yoann <yoann.deblois@gmail.com>
2022-03-09 17:52:46 +0100

    bugfix: added basename to router register in order to make get_queryset in views work.

--- epic_events/authentification/urls.py
+++ epic_events/authentification/urls.py
@@ -4,8 +4,8 @@ from rest_framework import routers
 from .views import UserViewSet, ClientViewSet
 
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'clients', ClientViewSet)
+router.register(r'users', UserViewSet, basename='UserViewSet')
+router.register(r'clients', ClientViewSet, basename='ClientViewSet')
 
 urlpatterns = [
     path('', include(router.urls))
