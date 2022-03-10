3fae642ff71e38c8a5c5197f6fcfb921cd91faad
Yoann <yoann.deblois@gmail.com>
2022-03-10 17:44:37 +0100

    func: Deletion of useless CreateEventSerializer.

--- epic_events/support/serializers.py
+++ epic_events/support/serializers.py
@@ -7,9 +7,3 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = ['id', 'support_contact', 'client', 'date_created', 'date_updated', 'status', 'attendees', 'event_date', 'notes']
-
-
-class CreateEventSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Event
-        fields = ['support_contact', 'client', 'status', 'attendees', 'event_date', 'notes']
