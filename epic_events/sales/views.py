06ac8f9dc73537f5684870614d81eb67d2a458a3
Yoann <yoann.deblois@gmail.com>
2022-03-10 17:36:07 +0100

    func: Using filterset class from filters.py, adding icontains, get and lte.

--- epic_events/sales/views.py
+++ epic_events/sales/views.py
@@ -3,6 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Contract
 from .serializers import ContractSerializer
+from .filters import ContractFilter
 from authentification.permissions import IsInSalesTeam, IsContractSClientResponsible
 
 
@@ -10,7 +11,7 @@ class ContractViewSet(viewsets.ModelViewSet):
     queryset = Contract.objects.all()
     serializer_class = ContractSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['client__last_name', 'client__email', 'date_created', 'amount']
+    filterset_class = ContractFilter
 
 
     def get_permissions(self):
