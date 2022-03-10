517b34a7be57b04e7482ad254152dc3f4746e1d6
Yoann <yoann.deblois@gmail.com>
2022-03-10 18:33:16 +0100

    func: Added sales_contact and client info to serializer.

--- epic_events/sales/serializers.py
+++ epic_events/sales/serializers.py
@@ -4,6 +4,14 @@ from .models import Contract
 
 
 class ContractSerializer(serializers.ModelSerializer):
+    client_first_name = serializers.CharField(read_only=True, source='client.first_name')
+    client_last_name = serializers.CharField(read_only=True, source='client.last_name')
+    client_email = serializers.CharField(read_only=True, source='client.email')
+    sales_contact = serializers.CharField(read_only=True, source='client.sales_contact')
+    sales_contact_first_name = serializers.CharField(read_only=True, source='client.sales_contact.first_name')
+    sales_contact_last_name = serializers.CharField(read_only=True, source='client.sales_contact.last_name')
+    sales_contact_email = serializers.CharField(read_only=True, source='client.sales_contact.email')
+
     class Meta:
         model = Contract
-        fields = ['id', 'client', 'date_created', 'date_updated', 'status', 'amount', 'payment_due']
+        fields = ['id', 'client', 'client_first_name', 'client_last_name', 'client_email', 'date_created', 'date_updated', 'status', 'amount', 'payment_due', 'sales_contact', 'sales_contact_first_name', 'sales_contact_last_name', 'sales_contact_email']
