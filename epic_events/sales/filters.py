846d634e93253a088467827d6654048b70d800fb
Yoann <yoann.deblois@gmail.com>
2022-03-10 17:34:52 +0100

    func: Filters for Contract.

---
+++ epic_events/sales/filters.py
@@ -0,0 +1,13 @@
+from django_filters import rest_framework as filters
+from .models import Contract
+
+
+class ContractFilter(filters.FilterSet):
+    class Meta:
+        model = Contract
+        fields = {
+            'client__last_name': ['exact', 'icontains'],
+            'client__email': ['exact', 'icontains'],
+            'date_created': ['exact', 'icontains', 'gte', 'lte'],
+            'amount': ['exact', 'gte', 'lte']
+        }
