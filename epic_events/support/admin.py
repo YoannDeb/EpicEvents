c8ef718bac9c40eae96b694c1059071f44806ab8
Yoann <yoann.deblois@gmail.com>
2022-03-10 11:58:57 +0100

    func: factorisation of retrieving of event and sales_contact.

--- epic_events/support/admin.py
+++ epic_events/support/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Event
+from .utils import get_event_from_admin_request, get_sales_contact_of_client_from_event
 
 
 class EventAdmin(admin.ModelAdmin):
@@ -29,10 +30,8 @@ class EventAdmin(admin.ModelAdmin):
     def get_readonly_fields(self, request, obj=None):
         read_only_fields = super().get_readonly_fields(request)
         try:
-            event_pk = int(request.resolver_match.kwargs['object_id'])
-            event = Event.objects.get(pk=event_pk)
-            client = event.client
-            sales_contact = client.sales_contact
+            event = get_event_from_admin_request(request)
+            sales_contact = get_sales_contact_of_client_from_event(event)
             if request.user != event.support_contact and request.user != sales_contact:
                 read_only_fields = read_only_fields + (
                     'client', 'support_contact', 'date_created', 'date_updated',
@@ -44,10 +43,8 @@ class EventAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         if request.resolver_match.url_name == 'support_event_change':
             try:
-                event_pk = int(request.resolver_match.kwargs['object_id'])
-                event = Event.objects.get(pk=event_pk)
-                client = event.client
-                sales_contact = client.sales_contact
+                event = get_event_from_admin_request(request)
+                sales_contact = get_sales_contact_of_client_from_event(event)
                 return request.user == event.support_contact or request.user == sales_contact or request.user.is_superuser
             except KeyError:
                 pass
