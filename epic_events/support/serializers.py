6b4eb66f9b03dbca58cc01aee853e032f70195d6
Yoann <yoann.deblois@gmail.com>
2022-03-10 18:33:27 +0100

    func: Added sales_contact and client info to serializer.

--- epic_events/support/serializers.py
+++ epic_events/support/serializers.py
@@ -4,6 +4,14 @@ from .models import Event
 
 
 class EventSerializer(serializers.ModelSerializer):
+    client_first_name = serializers.CharField(read_only=True, source='client.first_name')
+    client_last_name = serializers.CharField(read_only=True, source='client.last_name')
+    client_email = serializers.CharField(read_only=True, source='client.email')
+    support_contact = serializers.CharField(read_only=True, source='client.sales_contact')
+    support_contact_first_name = serializers.CharField(read_only=True, source='support_contact.first_name')
+    support_contact_last_name = serializers.CharField(read_only=True, source='support_contact.last_name')
+    support_contact_email = serializers.CharField(read_only=True, source='support_contact.email')
+
     class Meta:
         model = Event
-        fields = ['id', 'support_contact', 'client', 'date_created', 'date_updated', 'status', 'attendees', 'event_date', 'notes']
+        fields = ['id', 'client', 'date_created', 'date_updated', 'status', 'attendees', 'event_date', 'notes', 'support_contact', 'support_contact_first_name', 'support_contact_last_name', 'support_contact_email']
