8283abfc89bebd124ec0a369c85643a79dea6f5a
Yoann <yoann.deblois@gmail.com>
2022-03-10 17:43:50 +0100

    func: Deletion of useless CreateContractSerializer.

--- epic_events/sales/serializers.py
+++ epic_events/sales/serializers.py
@@ -7,9 +7,3 @@ class ContractSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contract
         fields = ['id', 'client', 'date_created', 'date_updated', 'status', 'amount', 'payment_due']
-
-
-class CreateContractSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Contract
-        fields = ['client', 'status', 'amount', 'payment_due']
