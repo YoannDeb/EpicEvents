9b84509c238afd155122e3e23d03823f89bc7425
Yoann <yoann.deblois@gmail.com>
2022-03-10 17:45:12 +0100

    func: Deletion of useless CreateClientSerializer.

--- epic_events/authentification/serializers.py
+++ epic_events/authentification/serializers.py
@@ -52,9 +52,3 @@ class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         fields = ['id', 'first_name', 'last_name', 'email', 'phone', 'mobile', 'date_created', 'date_updated', 'company_name', 'sales_contact']
-
-
-# class CreateClientSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Client
-#         fields = ['first_name', 'last_name', 'email', 'phone', 'mobile', 'company_name', 'sales_contact']
