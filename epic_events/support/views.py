41d90726612d6a0cacf7f4b5d4f1759a9f258b0a
Yoann <yoann.deblois@gmail.com>
2022-03-10 01:24:02 +0100

    func: added filters for api views

--- epic_events/support/views.py
+++ epic_events/support/views.py
@@ -1,20 +1,23 @@
 from rest_framework import viewsets, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Event
 from .serializers import EventSerializer
-from authentification.permissions import IsInSalesTeam, IsEventResponsible, IsEventSClientResponsible
+from authentification.permissions import IsInSupportTeam, IsEventResponsible, IsEventSClientResponsible
 
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['client__last_name', 'client__email', 'event_date']
 
     def get_permissions(self):
         permission_classes = [permissions.IsAuthenticated()]
         if self.action == 'retrieve' or self.action == 'list':
             permission_classes = [permissions.IsAuthenticated()]
         if self.action == 'create':
-            permission_classes = [permissions.IsAuthenticated(), IsInSalesTeam()]
+            permission_classes = [permissions.IsAuthenticated(), IsInSupportTeam()]
         if self.action == 'destroy' or self.action == 'update' or self.action == 'partial_update':
             permission_classes = [permissions.IsAuthenticated(), IsEventResponsible()]
         return permission_classes
