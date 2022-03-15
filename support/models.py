9def3d8df801843abaa7ec14dae20294d357558b
Yoann <yoann.deblois@gmail.com>
2022-03-15 14:10:55 +0100

    style: changed order of fields (support_contact is up).

--- support/models.py
+++ support/models.py
@@ -21,7 +21,6 @@ class Event(models.Model):
     ]
 
     client = models.ForeignKey(blank=True, null=True, to='authentication.CLIENT', on_delete=models.SET_NULL)
-    support_contact = models.ForeignKey(blank=True, null=True, to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
@@ -35,6 +34,7 @@ class Event(models.Model):
     attendees = models.IntegerField(blank=True)
     event_date = models.DateTimeField(blank=True)
     notes = models.TextField(max_length=5000, blank=True)
+    support_contact = models.ForeignKey(blank=True, null=True, to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL)
 
     def __str__(self):
         return str(self.client)
