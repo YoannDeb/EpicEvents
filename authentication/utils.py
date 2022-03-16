89b950b5b6f443ef1581b135ba1591a4eff3f608
Yoann <yoann.deblois@gmail.com>
2022-03-16 18:31:33 +0100

    Func: Factorisation of some repeated code in permissions.

--- authentication/utils.py
+++ authentication/utils.py
@@ -6,3 +6,19 @@ def get_sales_contact_from_admin_request(request):
     client = Client.objects.get(pk=client_pk)
     sales_contact = client.sales_contact
     return sales_contact
+
+
+def get_sales_contact_from_event_or_contract(self, object, logger):
+    try:
+        client = object.client
+    except AttributeError as e:
+        self.message = e
+        logger.warning(f"The event has no client!: {e}")
+        return False
+    try:
+        sales_contact = client.sales_contact
+    except AttributeError as e:
+        self.message = e
+        logger.warning(f"The client has no sales_contact!: {e}")
+        return False
+    return sales_contact
