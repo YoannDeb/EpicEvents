ce958fb7fbe8be6248f5ec8c6bc19d7347e4bf57
Yoann <yoann.deblois@gmail.com>
2022-03-17 10:34:33 +0100

    docs: Added comments.

--- authentication/utils.py
+++ authentication/utils.py
@@ -2,6 +2,11 @@ from .models import Client
 
 
 def get_sales_contact_from_admin_request(request):
+    """
+    Factorisation retrieving sales contact of a client, used several times in admin.py.
+    :param request: The HTML request.
+    :return: A sales contact (CustomUser object).
+    """
     client_pk = int(request.resolver_match.kwargs['object_id'])
     client = Client.objects.get(pk=client_pk)
     sales_contact = client.sales_contact
@@ -9,6 +14,11 @@ def get_sales_contact_from_admin_request(request):
 
 
 def get_sales_contact_from_event_or_contract(self, object, logger):
+    """
+    Factorisation retrieving sales contact of the client of a contract or event.
+    Used several times in permissions.py.
+    :return: A sales contact (CustomUser object).
+    """
     try:
         client = object.client
     except AttributeError as e:
