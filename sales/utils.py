ff4cb6657c700c0f4927a85e0b2a15164003fa3f
Yoann <yoann.deblois@gmail.com>
2022-03-17 10:46:39 +0100

    docs: Added comments.

--- sales/utils.py
+++ sales/utils.py
@@ -2,6 +2,11 @@ from .models import Contract
 
 
 def get_sales_contact_of_client_of_contract_from_admin_request(request):
+    """
+    Factorisation retrieving sales contact of the client of a contract, used several times in admin.py.
+    :param request: The HTML request.
+    :return: A sales contact (CustomUser object).
+    """
     contract_pk = int(request.resolver_match.kwargs['object_id'])
     contract = Contract.objects.get(pk=contract_pk)
     client = contract.client
--- support/utils.py
+++ support/utils.py
@@ -2,12 +2,22 @@ from .models import Event
 
 
 def get_event_from_admin_request(request):
+    """
+    Factorisation retrieving an event from a request, used several times in admin.py.
+    :param request: The HTML request.
+    :return: A sales contact (CustomUser object).
+    """
     event_pk = int(request.resolver_match.kwargs['object_id'])
     event = Event.objects.get(pk=event_pk)
     return event
 
 
 def get_sales_contact_of_client_from_event(event):
+    """
+    Factorisation retrieving sales contact of the client of an event, used several times in admin.py.
+    :param event: An Event object.
+    :return: A sales contact (CustomUser object).
+    """
     client = event.client
     sales_contact = client.sales_contact
     return sales_contact
