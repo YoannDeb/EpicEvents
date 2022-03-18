dec25cae2c95264e9cd33cda45506cb8336e5c38
Yoann <yoann.deblois@gmail.com>
2022-03-18 16:29:26 +0100

    func: Logout in API by jwt refresh token blacklisting.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import ClientViewSet
+from .views import ClientViewSet, LogOutEverywhereAPIView
 
 router = routers.DefaultRouter()
 router.register(r'clients', ClientViewSet, basename='ClientViewSet')
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('logouteverywhere/', LogOutEverywhereAPIView.as_view())
 ]
